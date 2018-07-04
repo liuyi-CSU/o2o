c58b072c12d18bb923d26cb92c63eef62109fd51
liuyi-CSU <408827065@qq.com>
2018-07-04 22:46:21 +0800

    again

---
+++ src/main/java/com/imooc/o2o/entity/PersonInfo.java
@@ -0,0 +1,81 @@
+package com.imooc.o2o.entity;
+
+import java.sql.Date;
+
+/**用户信息
+ * @author liuyi
+ *
+ */
+public class PersonInfo {
+
+	private Long userId;
+	private String name;
+	private String profileImg;
+	private String email;
+	private String gender;
+	private Integer enableStatus;
+	private Integer userTyper;
+	private Integer userTime;
+	private Date createTime;
+	private Date lastEditTime;
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProfileImg() {
+		return profileImg;
+	}
+	public void setProfileImg(String profileImg) {
+		this.profileImg = profileImg;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Integer getEnableStatus() {
+		return enableStatus;
+	}
+	public void setEnableStatus(Integer enableStatus) {
+		this.enableStatus = enableStatus;
+	}
+	public Integer getUserTyper() {
+		return userTyper;
+	}
+	public void setUserTyper(Integer userTyper) {
+		this.userTyper = userTyper;
+	}
+	public Integer getUserTime() {
+		return userTime;
+	}
+	public void setUserTime(Integer userTime) {
+		this.userTime = userTime;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public Date getLastEditTime() {
+		return lastEditTime;
+	}
+	public void setLastEditTime(Date lastEditTime) {
+		this.lastEditTime = lastEditTime;
+	}
+}
