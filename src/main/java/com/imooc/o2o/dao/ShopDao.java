cba282d81c4db97b68541cdf14e96ad450375ccf
liuyi-CSU <408827065@qq.com>
2018-09-11 00:50:22 +0800

    begin-6-7

--- src/main/java/com/imooc/o2o/dao/ShopDao.java
+++ src/main/java/com/imooc/o2o/dao/ShopDao.java
@@ -9,6 +9,41 @@ import com.imooc.o2o.entity.Shop;
 public interface ShopDao {
 
 	/**
+	 * 分页查询店铺,可输入的条件有：店铺名（模糊），店铺状态，店铺Id,店铺类别,区域ID
+	 * 
+	 * @param shopCondition
+	 * @param rowIndex
+	 * @param pageSize
+	 * @return
+	 */
+	List<Shop> queryShopList(@Param("shopCondition") Shop shopCondition,
+			@Param("rowIndex") int rowIndex, @Param("pageSize") int pageSize);
+
+	/**
+	 * 返回queryShopList总数
+	 * 
+	 * @param shopCondition
+	 * @return
+	 */
+	int queryShopCount(@Param("shopCondition") Shop shopCondition);
+
+	/**
+	 * 通过employee id 查询店铺
+	 * 
+	 * @param employeeId
+	 * @return List<shop>
+	 */
+	List<Shop> queryByEmployeeId(long employeeId);
+
+	/**
+	 * 通过owner id 查询店铺
+	 * 
+	 * @param shopId
+	 * @return shop
+	 */
+	Shop queryByShopId(long shopId);
+
+	/**
 	 * 新增店铺
 	 * 
 	 * @param shop
@@ -18,9 +53,18 @@ public interface ShopDao {
 
 	/**
 	 * 更新店铺信息
+	 * 
 	 * @param shop
-	 * @return
+	 * @return effectedNum
 	 */
 	int updateShop(Shop shop);
+
+	/**
+	 * 删除店铺（初版，即只支持删除尚且没有发布商品的店铺）
+	 * 
+	 * @param shopName
+	 * @return effectedNum
+	 */
+	int deleteShopByName(String shopName);
 
 }
--- src/main/java/com/imooc/o2o/util/ImageUtil.java
+++ src/main/java/com/imooc/o2o/util/ImageUtil.java
@@ -4,10 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
+
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import net.coobird.thumbnailator.Thumbnails;
 import net.coobird.thumbnailator.geometry.Positions;
@@ -19,21 +23,59 @@ public class ImageUtil {
 	/**
 	 * 处理缩略图,并返回新生成图片的相对值路径
 	 */
-	public static String generateThumbnail(InputStream thumbnailInputStream,String fileName, String targetAddr) {
-		
-		String realFileName = getRandomFileName();
-		String extension = getFileExtension(fileName);
+	public static String generateThumbnail(CommonsMultipartFile thumbnail, String targetAddr) {
+		String realFileName = FileUtil.getRandomFileName();
+		String extension = getFileExtension(thumbnail);
 		makeDirPath(targetAddr);
 		String relativeAddr = targetAddr + realFileName + extension;
-		File dest = new File(PathUtil.getImgBasePath() + relativeAddr);
+		File dest = new File(FileUtil.getImgBasePath() + relativeAddr);
 		try {
-			Thumbnails.of(thumbnailInputStream).size(200, 200)
-			.watermark(Positions.BOTTOM_RIGHT, ImageIO.read(new File(basePath + "watermark.jpg")), 0.25f)
-			.outputQuality(0.8f).toFile(dest);
-		} catch(IOException e) {
-			e.printStackTrace();
+			Thumbnails.of(thumbnail.getInputStream()).size(200, 200).outputQuality(0.25f).toFile(dest);
+		} catch (IOException e) {
+			throw new RuntimeException("创建缩略图失败：" + e.toString());
 		}
 		return relativeAddr;
+	}
+	
+	public static String generateNormalImg(CommonsMultipartFile thumbnail, String targetAddr) {
+		String realFileName = FileUtil.getRandomFileName();
+		String extension = getFileExtension(thumbnail);
+		makeDirPath(targetAddr);
+		String relativeAddr = targetAddr + realFileName + extension;
+		File dest = new File(FileUtil.getImgBasePath() + relativeAddr);
+		try {
+			Thumbnails.of(thumbnail.getInputStream()).size(337, 640).outputQuality(0.5f).toFile(dest);
+		} catch (IOException e) {
+			throw new RuntimeException("创建缩略图失败：" + e.toString());
+		}
+		return relativeAddr;
+	}
+
+	public static List<String> generateNormalImgs(List<CommonsMultipartFile> imgs, String targetAddr) {
+		int count = 0;
+		List<String> relativeAddrList = new ArrayList<String>();
+		if (imgs != null && imgs.size() > 0) {
+			makeDirPath(targetAddr);
+			for (CommonsMultipartFile img : imgs) {
+				String realFileName = FileUtil.getRandomFileName();
+				String extension = getFileExtension(img);
+				String relativeAddr = targetAddr + realFileName + count + extension;
+				File dest = new File(FileUtil.getImgBasePath() + relativeAddr);
+				count++;
+				try {
+					Thumbnails.of(img.getInputStream()).size(600, 300).outputQuality(0.5f).toFile(dest);
+				} catch (IOException e) {
+					throw new RuntimeException("创建图片失败：" + e.toString());
+				}
+				relativeAddrList.add(relativeAddr);
+			}
+		}
+		return relativeAddrList;
+	}
+	
+	private static String getFileExtension(CommonsMultipartFile cFile) {
+		String originalFileName = cFile.getOriginalFilename();
+		return originalFileName.substring(originalFileName.lastIndexOf("."));
 	}
 	
 	/**
@@ -54,9 +96,9 @@ public class ImageUtil {
 	 * @param thumbnail
 	 * @return
 	 */
-	private static String getFileExtension(String fileName) {
-		return fileName.substring(fileName.lastIndexOf("."));
-	}
+//	private static String getFileExtension(String fileName) {
+//		return fileName.substring(fileName.lastIndexOf("."));
+//	}
 	/**
 	 * 生成随机文件名，当前年月日小时分钟+五位随机数
 	 * @return
